7cb32e55f59fc0902e7a5809027ced8ca24855fe
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2020-08-31 17:12:18 -0400

    color + spacing changes

--- src/components/WorkWindow.vue
+++ src/components/WorkWindow.vue
@@ -4,7 +4,7 @@
         <hr class="mx-6">
         <div class="m-2 p-2">
             <h2 class="font-semibold text-xl text-gray-800 ml-2">Rethink Solutions Inc. </h2>
-            <p class="mx-2 text-gray-600 uppercase font-semibold tracking-wide text-xs"> <span class="text-teal-400">Software Developer </span>&bull; May 2020 - September 2020</p>
+            <p class="mx-2 text-gray-600 uppercase font-semibold tracking-wide text-xs"> <span class="text-teal-400">Software Developer </span>&middot; May 2020 - September 2020</p>
             <p class="m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consequuntur, ad beatae provident ea fugit! Corrupti 
                 quas tenetur explicabo sunt in rerum sed officia possimus qui! Quo eum aut dignissimos? Lorem, ipsum dolor sit amet consectetur 
                 adipisicing elit. Cum assumenda dignissimos quasi voluptatem necessitatibus qui eaque perferendis magnam, beatae, saepe autem error rem, 
