b9ac9d6ef6238431c605117908471c143308ed3c
Ezra Katzman <47403054+EzraKatzman@users.noreply.github.com>
2020-09-01 11:30:14 -0400

    button edits, navbar changes

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -2,6 +2,14 @@
     <div class="m-2 p-2 text-center text-teal-400 font-medium md:border-2 md:border-teal-400 md:rounded-lg lg:shadow-2xl md:bg-gray-100">
         <img src="@\assets\images\shattered.png" alt="Please appear" class="m-2 inline-block h-20 w-20 lg:h-40 lg:w-40 justify-middle rounded-full">
         <p class="p-2 lg:text-lg">I'm a Software Developer who loves creating programs that solve problems and look good doing it.</p>
+        <div class="hidden lg:block m-2">
+            <a href="" target="_blank"><svg viewBox="0 0 20 20" fill="currentColor" class="inline w-10 h-10 hover:text-teal-700">
+                <path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg></a>
+            <a href="" target="_blank"><svg viewBox="0 0 448 512" fill="currentColor" class="inline w-10 h-10 hover:text-teal-700">
+                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a>
+            <a href="https://github.com/EzraKatzman" target="_blank"><svg viewBox="0 0 24 24" fill="currentColor" class="inline w-10 h-10 hover:text-teal-700">
+                <path d="M12 1A10.89 10.89 0 001 11.77 10.79 10.79 0 008.52 22c.55.1.75-.23.75-.52v-1.83c-3.06.65-3.71-1.44-3.71-1.44a2.86 2.86 0 00-1.22-1.58c-1-.66.08-.65.08-.65a2.31 2.31 0 011.68 1.11 2.37 2.37 0 003.2.89 2.33 2.33 0 01.7-1.44c-2.44-.27-5-1.19-5-5.32a4.15 4.15 0 011.11-2.91 3.78 3.78 0 01.11-2.84s.93-.29 3 1.1a10.68 10.68 0 015.5 0c2.1-1.39 3-1.1 3-1.1a3.78 3.78 0 01.11 2.84A4.15 4.15 0 0119 11.2c0 4.14-2.58 5.05-5 5.32a2.5 2.5 0 01.75 2v2.95c0 .35.2.63.75.52A10.8 10.8 0 0023 11.77 10.89 10.89 0 0012 1" data-name="github"/></svg></a>
+        </div>
     </div>
 </template>
 
